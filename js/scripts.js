5417dca4c4325e1546f5572ac7b16f45612f5cf9
Adrian Escalante <adresc@gmail.com>
2022-07-06 20:41:58 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+console.log('Ejemplo extra');
+
+function promedio() {
+    const TOTAL_ITERACIONES = 3
+    let total = 0
+
+    for (let i = 0; i < TOTAL_ITERACIONES; i++) {
+        console.log('iteración ' + i);
+        total = total + parseInt( prompt('Dame una calificación') )
+    }
+
+    let promedio = total / TOTAL_ITERACIONES
+    alert('El promedio es: ' + promedio)
+}
+
+let comando = prompt('¿Qué opción quieres hacer? (promedio, suma, multiplicacion)')
+
+switch ( comando ) {
+    case 'promedio':
+        promedio()
+        break;
+    case 'suma':
+        alert('ejecutando la suma ... ')
+        break;
+    case 'multiplicacion':
+        alert('ejecutando la multiplicación ... ')
+        break;
+
+    default:
+        alert('Comando no válido')
+        break;
+}
